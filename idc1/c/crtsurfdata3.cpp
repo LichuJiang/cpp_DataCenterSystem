825d13757bfb17508e4e69477cba459d116c0a07
root <root@45-79-180-158.ip.linodeusercontent.com>
2022-07-16 18:14:00 +0000

    step 3

--- idc1/c/crtsurfdata3.cpp
+++ idc1/c/crtsurfdata3.cpp
@@ -17,6 +17,25 @@ vector<struct st_stcode> vstcode;
 // 把站点参数文件中加载到vstcode容器中。
 bool LoadSTCode(const char *inifile);
 
+// 全国气象站点分钟观测数据结构
+struct st_surfdata
+{
+  char obtid[11];      // 站点代码。
+  char ddatetime[21];  // 数据时间：格式yyyymmddhh24miss
+  int  t;              // 气温：单位，0.1摄氏度。
+  int  p;              // 气压：0.1百帕。
+  int  u;              // 相对湿度，0-100之间的值。
+  int  wd;             // 风向，0-360之间的值。
+  int  wf;             // 风速：单位0.1m/s
+  int  r;              // 降雨量：0.1mm。
+  int  vis;            // 能见度：0.1米。
+};
+
+vector<struct st_surfdata>vsurfdata; //存放全国气象战点分钟观测数据容器
+
+//模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中
+void CrtSurfData();
+
 CLogFile logfile;    // 日志类。
 
 int main(int argc,char *argv[])
@@ -25,7 +44,7 @@ int main(int argc,char *argv[])
   {
     // 如果参数非法，给出帮助文档。
     printf("Using:./crtsurfdata3 inifile outpath logfile\n");
-    printf("Example:/project/idc1/bin/crtsurfdata3 /project/idc1/ini/stcode.ini /tmp/surfdata /log/idc/crtsurfdata2.log\n\n");
+    printf("Example:/project/idc1/bin/crtsurfdata3 /project/idc1/ini/stcode.ini /tmp/surfdata /log/idc/crtsurfdata3.log\n\n");
 
     printf("inifile 全国气象站点参数文件名。\n");
     printf("outpath 全国气象站点数据文件存放的目录。\n");
@@ -44,6 +63,9 @@ int main(int argc,char *argv[])
 
   // 把站点参数文件中加载到vstcode容器中。 
   if (LoadSTCode(argv[1])==false) return -1;
+
+  //模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中
+  CrtSurfData();
 
 
   logfile.WriteEx("crtsurfdata3 运行结束。\n");
@@ -96,4 +118,27 @@ bool LoadSTCode(const char *inifile)
     logfile.Write("provname=%s,obtid=%s,obtname=%s,lat=%.2f,lon=%.2f,height=%.2f\n",vstcode[ii].provname,vstcode[ii].obtid,vstcode[ii].obtname,vstcode[ii].lat,vstcode[ii].lon,vstcode[ii].height);
 
   return true;
+}
+
+void CrtSurfData(){
+  srand(time(0));
+  char strddatetime[21];
+  memset(strddatetime,0,sizeof(strddatetime));
+  LocalTime(strddatetime,"yyyymmddhh24miss");
+  
+  struct st_surfdata stsurfdata;
+  for(int ii=0;ii<vstcode.size();ii++){
+    memset(&stsurfdata,0,sizeof(struct st_surfdata));
+    strncpy(stsurfdata.obtid,vstcode[ii].obtid,10); // 站点代码。
+    strncpy(stsurfdata.ddatetime,strddatetime,14);  // 数据时间：格式yyyymmddhh24miss
+    stsurfdata.t=rand()%351;       // 气温：单位，0.1摄氏度
+    stsurfdata.p=rand()%265+10000; // 气压：0.1百帕
+    stsurfdata.u=rand()%100+1;     // 相对湿度，0-100之间的值。
+    stsurfdata.wd=rand()%360;      // 风向，0-360之间的值。
+    stsurfdata.wf=rand()%150;      // 风速：单位0.1m/s
+    stsurfdata.r=rand()%16;        // 降雨量：0.1mm
+    stsurfdata.vis=rand()%5001+100000;  // 能见度：0.1米
+
+    vsurfdata.push_back(stsurfdata);
+  }
 }
